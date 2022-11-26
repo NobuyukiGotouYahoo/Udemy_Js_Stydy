4ef9fd0abb6a3bf07e5bede286473308b889e898
NobuyukiGotouYahoo <119094014+NobuyukiGotouYahoo@users.noreply.github.com>
2022-11-26 17:54:10 +0900

    とりあえず第一章終わり

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-import "./styles.css";
+/**
+ * const let等の変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "上書き";
+// console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+
+val2 = "内容書き換え";
+console.log(val2);
+
+const valObejct = {
+  name: "ゴトウ",
+  age: 24
+};
+
+console.log(valObejct);
+
+console.log(valObejct.name);
+console.log(valObejct.age);
+
+valObejct.age = 27;
+
+console.log(valObejct.age);
+
+const valArray = ["dog", "cat"];
+console.log(valArray);
+console.log(valArray[0]);
+
+valArray[0] = "monky";
+valArray[1] = "cow";
+console.log(valArray);
+console.log(valArray[0]);
+
+const mesageTemplate = `テンプレート機能を使用しています.\n 名前は${valObejct.name}です。\n 年齢は${valObejct.age}です。`;
+console.log(mesageTemplate);
+
+const { name, age } = valObejct;
+const mesageTemplate2 = `テンプレート機能を使用しています。\n 名前は${name}です。\n 年齢は${age}です。`;
+console.log(mesageTemplate2);
